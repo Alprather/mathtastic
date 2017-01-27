7b9d2cdb837e36dd40f91eacce7610e374fb799b
Alicia Prather <Alprather@gmail.com>
2017-01-26 20:28:37 -0600

    Classroom tools added

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   get "welcome/index"
 
   get "welcome/about"
-  get "welcome/lessons" 
+  get "welcome/lessons"
   get "fractions/index"
+  get "welcome/classroom"
   root 'welcome#index'
 
 end
