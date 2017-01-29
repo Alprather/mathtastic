11c9f7e62f36bc7ef459b7e2ad261adddafa9712
Alicia Prather <Alprather@gmail.com>
2017-01-29 11:13:18 -0600

    User errors fixed start over

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170128214515) do
+ActiveRecord::Schema.define(version: 20170129160155) do
 
   create_table "lessons", force: :cascade do |t|
     t.string   "title"
