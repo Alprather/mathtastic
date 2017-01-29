ed3ada8a6feea0cbfb2c2ca3310f9b193b72d27f
Alicia Prather <Alprather@gmail.com>
2017-01-29 09:06:22 -0600

    lessons updated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170128204651) do
+ActiveRecord::Schema.define(version: 20170128214515) do
 
   create_table "lessons", force: :cascade do |t|
     t.string   "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20170128204651) do
     t.string   "lesson_file"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "description"
   end
 
 end
