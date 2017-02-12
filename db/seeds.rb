287267eba2357c9adbabb91a8357164fc00855f3
Alicia Prather <Alprather@gmail.com>
2017-02-12 10:03:44 -0600

    User roles added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+admin = User.create!(
+
+   email:    'alprather@gmail.com',
+   password: 'AdminAlicia99',
+   role:     'admin'
+ )
+ 
